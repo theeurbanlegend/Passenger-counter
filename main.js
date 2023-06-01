c2f4bbbf08143dbd3f0c5132847e889ef0c52178
theeurbanlegend <mbuto152@gmail.com>
2023-06-01 16:33:48 +0300

    Adding increment message below buttons

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ function increment() {
     inc=inc+1;
     countY.innerText = inc;
  }
+let saveEl = document.getElementById("save-el")
+function save(){
+    console.log(inc)
+    saveEl.innerText=`${inc} - `
+}
